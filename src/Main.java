6993a6fdfbb37e98b9f21803dcbcb54a98e22905
sukruoktay <oktaysukru@hotmail.com>
2025-03-12 17:58:32 +0300

    güncel

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        double kilo = 105.0;
-        double boy  = 1.72;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Lütfen boyunuzu (metre cinsinde) giriniz");
+        double boy = input.nextDouble();
+        System.out.println("Lütfen kilonuzu giriniz");
+        double kilo = input.nextDouble();
 
         double vucutKitleIndeksi = kilo / (boy*boy);
 
-        System.out.println("Lütfen boyunuzu (metre cinsinde) giriniz : " +boy);
-        System.out.println("Lütfen kilonuzu giriniz : " +kilo);
         System.out.println("Vücut Kitle İndeksiniz : " +vucutKitleIndeksi );
 
     }
